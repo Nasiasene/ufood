e8083c7854d8206ff81d78e78f9a061c570fe3be
Ana Cecília <anaceciliabezerra10@gmail.com>
2026-02-04 17:00:51 -0300

    refactor: change the validation to control class

--- src/controllers/user_control.py
+++ src/controllers/user_control.py
@@ -10,16 +10,19 @@ class UserControl:
         
     def sign_up(self, user_data: UserCreateSchema):
         user_type = UserType.USER if user_data.user_type.value == "user" else UserType.ADMIN
-        
-        new_user = User(
-            name=user_data.name,
-            email=user_data.email,
-            user_type=user_data.user_type,
-            login=user_data.login,
-            phone=user_data.phone,
-            password=user_data.password
-        )
-        
+        try: 
+            user_data.validate_login()
+            user_data.validate_password()
+            new_user = User(
+                name=user_data.name,
+                email=user_data.email,
+                user_type=user_type,
+                login=user_data.login,
+                phone=user_data.phone,
+                password=user_data.password
+            )
+        except ValueError as e:
+            raise ValueError(str(e))    
         return self._repository.add(new_user)
     
     def list_users(self) -> List[User]:
